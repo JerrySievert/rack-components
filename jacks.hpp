77c25bf70ae8f0d680819819e2504f35c48e19f7
Jerry Sievert <code@legitimatesounding.com>
2019-07-25 18:19:37 -0700

    update port code

--- jacks.hpp
+++ jacks.hpp
@@ -7,26 +7,48 @@ using namespace rack;
 
 extern Plugin *pluginInstance;
 
-struct RCJackSmallRed : app::SvgPort {
+struct LocalPort : PortWidget {
+	widget::FramebufferWidget *fb;
+	widget::SvgWidget *sw;
+
+  LocalPort() {
+  	fb = new widget::FramebufferWidget;
+  	addChild(fb);
+
+  	sw = new widget::SvgWidget;
+  	fb->addChild(sw);
+  }
+
+  void setSvg(std::shared_ptr<Svg> svg) {
+  	sw->setSvg(svg);
+  	fb->box.size = sw->box.size;
+  	box.size = sw->box.size;
+
+  	fb->dirty = true;
+  }
+
+};
+
+struct RCJackSmallRed : LocalPort {
   RCJackSmallRed() {
     setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/JackSmallRed.svg")));
   }
 };
 
-struct RCJackSmallGrey : app::SvgPort {
+struct RCJackSmallGrey : LocalPort {
   RCJackSmallGrey() {
     setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/JackSmallGrey.svg")));
   }
 };
 
-struct RCJackSmallDark : app::SvgPort {
+struct RCJackSmallDark : LocalPort {
   RCJackSmallDark() {
     setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/JackSmallDark.svg")));
   }
 };
 
 
-struct RCJackSmallLight : app::SvgPort {
+struct RCJackSmallLight : LocalPort {
   RCJackSmallLight() {
     setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/JackSmallLight.svg")));
   }
